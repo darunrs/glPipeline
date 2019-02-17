20a20faf05948d1a9e0ee1c4d5e5dfb00d8a0421
Darun Seethammagari <dseet001@bolt.cs.ucr.edu>
2019-02-17 11:39:33 -0800

    code compiles and runs, wrong triangle

--- driver_state.cpp
+++ driver_state.cpp
@@ -20,7 +20,7 @@ void initialize_render(driver_state& state, int width, int height)
     state.image_height=height;
     state.image_color=0;
     state.image_depth=0;
-    // std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
+    std::cout<<"TODO: allocate and initialize state.image_color and state.image_depth."<<std::endl;
     state.image_color = new pixel[width * height];
     state.image_depth = new float[width * height];
     std::fill(state.image_color, state.image_color + (width * height), make_pixel(0,0,0));
@@ -35,12 +35,13 @@ void initialize_render(driver_state& state, int width, int height)
 //   render_type::strip -    The vertices are to be interpreted as a triangle strip.
 void render(driver_state& state, render_type type)
 {
-    std::cout<<"TODO: implement rendering."<<std::endl;
+    // std::cout<<"TODO: implement rendering."<<std::endl;
     for (int i = 0; i < state.num_vertices; i = i + 3) {
 		data_geometry* pass[3];
 		for (int j = 0; j < 3; j++) {
 			int ind = ((i + j) * state.floats_per_vertex);
-			pass[j]->data = state.vertex_data + ind;
+			pass[j] = new data_geometry();
+      pass[j]->data = state.vertex_data + ind;
 		}
 		rasterize_triangle(state, pass);
 	}
@@ -67,30 +68,51 @@ void clip_triangle(driver_state& state, data_geometry* in[3],int face)
 // fragments, calling the fragment shader, and z-buffering.
 void rasterize_triangle(driver_state& state, data_geometry* in[3])
 {
-    std::cout<<"TODO: implement rasterization"<<std::endl;
+    // std::cout<<"TODO: implement rasterization"<<std::endl;
     for(int i = 0; i < 3; i++) {
-		data_vertex dv;
-		dv.data =  in[i]->data;
-		state.vertex_shader(dv, *in[i], state.uniform_data);
-		for (int j = 0; j < 3; j++) {
-			in[i]->gl_Position[j] = in[i]->gl_Position[j] / in[i]->gl_Position[3];
-		}
-		in[i]->gl_Position[0] = (in[i]->gl_Position[0] + 1) * (state.image_width / 2);
-		in[i]->gl_Position[1] = (in[i]->gl_Position[1] + 1) * (state.image_height / 2);	
-		int image_index = (in[i]->gl_Position[0] * state.image_width) + state.image_height;	
-		state.image_color[image_index].make_pixel(255, 255, 255);
-	}
-	float area = 0.5 * ((in[0]->data[0] * (in[1]->data[1] - in[2]->data[1])) + 
-						(in[1]->data[0] * (in[2]->data[1] - in[0]->data[1])) + 
-						(in[2]->data[0] * (in[0]->data[1] - in[1]->data[1])));
-	float Area = 0.5 * ((in[0]->data[0] * (in[1]->data[1] - in[2]->data[1])) + 
-						(in[1]->data[0] * (in[2]->data[1] - in[0]->data[1])) + 
-						(in[2]->data[0] * (in[0]->data[1] - in[1]->data[1])));
-	float Brea = 0.5 * ((in[0]->data[0] * (in[1]->data[1] - in[2]->data[1])) + 
-						(in[1]->data[0] * (in[2]->data[1] - in[0]->data[1])) + 
-						(in[2]->data[0] * (in[0]->data[1] - in[1]->data[1])));
-	float Grea = 0.5 * ((in[0]->data[0] * (in[1]->data[1] - in[2]->data[1])) + 
-						(in[1]->data[0] * (in[2]->data[1] - in[0]->data[1])) + 
-						(in[2]->data[0] * (in[0]->data[1] - in[1]->data[1])));
+  		data_vertex dv;
+  		dv.data =  in[i]->data;
+  		state.vertex_shader(dv, *in[i], state.uniform_data);
+  		for (int j = 0; j < 3; j++) {
+  			in[i]->gl_Position[j] = in[i]->gl_Position[j] / in[i]->gl_Position[3];
+  		}
+  		in[i]->gl_Position[0] = (in[i]->gl_Position[0] + 1) * (state.image_width / 2);
+  		in[i]->gl_Position[1] = (in[i]->gl_Position[1] + 1) * (state.image_height / 2);	
+  		int image_index = ((state.image_width - in[i]->gl_Position[0]) * state.image_width) + in[i]->gl_Position[1];
+  		state.image_color[image_index] = make_pixel(255, 255, 255);	
+  	}
+    for (int i = 0; i < 3; i++) {
+        std::cout << "(" << in[i]->gl_Position[0] << ", " << in[i]->gl_Position[1] << ")" << std::endl;
+    }
+	float area = 0.5 * ((in[0]->gl_Position[0] * (in[1]->gl_Position[1] - in[2]->gl_Position[1])) + 
+						          (in[1]->gl_Position[0] * (in[2]->gl_Position[1] - in[0]->gl_Position[1])) + 
+						          (in[2]->gl_Position[0] * (in[0]->gl_Position[1] - in[1]->gl_Position[1])));
+                                     
+  int startX = std::min(std::min(in[0]->gl_Position[0], in[1]->gl_Position[0]), in[2]->gl_Position[0]);
+  int endX = std::max(std::max(in[0]->gl_Position[0], in[1]->gl_Position[0]), in[2]->gl_Position[0]);
+  int startY = std::min(std::min(in[0]->gl_Position[1], in[1]->gl_Position[1]), in[2]->gl_Position[1]);
+  int endY = std::max(std::max(in[0]->gl_Position[1], in[1]->gl_Position[1]), in[2]->gl_Position[1]);
+  
+  for (int i = startX; i < endX; i++) {
+      for (int j = startY; j < endY; j++) {
+          	/*float Area = 0.5 * ((i * (in[1]->gl_Position[1] - in[2]->gl_Position[1])) + 
+          						          (in[1]->gl_Position[0] * (in[2]->gl_Position[1] - j)) + 
+          						          (in[2]->gl_Position[0] * (j - in[1]->gl_Position[1])));*/
+          	float Brea = 0.5 * ((in[0]->gl_Position[0] * (j - in[2]->gl_Position[1])) + 
+          						          (i * (in[2]->gl_Position[1] - in[0]->gl_Position[1])) + 
+          						          (in[2]->gl_Position[0] * (in[0]->gl_Position[1] - j)));
+          	float Grea = 0.5 * ((in[0]->gl_Position[0] * (in[1]->gl_Position[1] - j)) + 
+          						          (in[1]->gl_Position[0] * (j - in[0]->gl_Position[1])) + 
+          						          (i * (in[0]->gl_Position[1] - in[1]->gl_Position[1])));
+           //double alpha = Area / area;
+           double beta = Brea / area;
+           double gamma = Grea / area;
+           double alpha = (1 - beta) - gamma;
+           if (alpha >= 0 && beta >= 0 && gamma >= 0 && (alpha + beta + gamma) <= 1.001) {
+               int image_index = ((state.image_width - i) * state.image_width) + j;
+  		         state.image_color[image_index] = make_pixel(255, 255, 255);
+           }
+      }
+  }
 }
 
