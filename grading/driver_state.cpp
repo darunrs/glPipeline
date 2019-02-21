18fd6a85214792b012f3387aada83e30aec14ec7
Darun Seethammagari <dseet001@wch110-15.cs.ucr.edu>
2019-02-21 10:54:20 -0800

    fix it

--- grading/driver_state.cpp
+++ grading/driver_state.cpp
@@ -37,11 +37,15 @@ void render(driver_state& state, render_type type)
 {
     // std::cout<<"TODO: implement rendering."<<std::endl;
     for (int i = 0; i < state.num_vertices; i = i + 3) {
-		data_geometry* pass[3];
+		const data_geometry* pass[3];
 		for (int j = 0; j < 3; j++) {
 			int ind = ((i + j) * state.floats_per_vertex);
-			pass[j] = new data_geometry();
-      pass[j]->data = state.vertex_data + ind;
+			data_geometry* dg = new data_geometry();
+      			dg->data = state.vertex_data + ind;
+			data_vertex dv;
+			dv.data = dg->data;
+			state.vertex_shader(dv, *dg, state.uniform_data);
+			pass[j] = dg;
 		}
 		rasterize_triangle(state, pass);
 	}
@@ -52,7 +56,7 @@ void render(driver_state& state, render_type type)
 // It will be called recursively, once for each clipping face (face=0, 1, ..., 5) to
 // clip against each of the clipping faces in turn.  When face=6, clip_triangle should
 // simply pass the call on to rasterize_triangle.
-void clip_triangle(driver_state& state, data_geometry* in[3],int face)
+void clip_triangle(driver_state& state, const data_geometry* in[3],int face)
 {
     if(face==6)
     {
@@ -66,48 +70,54 @@ void clip_triangle(driver_state& state, data_geometry* in[3],int face)
 // Rasterize the triangle defined by the three vertices in the "in" array.  This
 // function is responsible for rasterization, interpolation of data to
 // fragments, calling the fragment shader, and z-buffering.
-void rasterize_triangle(driver_state& state, data_geometry* in[3])
+void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 {
     // std::cout<<"TODO: implement rasterization"<<std::endl;
+    int inn[3][3] = {0};
     for(int i = 0; i < 3; i++) {
-  		data_vertex dv;
-  		dv.data =  in[i]->data;
-  		state.vertex_shader(dv, *in[i], state.uniform_data);
   		for (int j = 0; j < 3; j++) {
-  			in[i]->gl_Position[j] = in[i]->gl_Position[j] / in[i]->gl_Position[3];
+			inn[i][j] = in[i]->gl_Position[j] / in[i]->gl_Position[3];
+  			//in[i]->gl_Position[j] = in[i]->gl_Position[j] / in[i]->gl_Position[3];
   		}
-  		in[i]->gl_Position[0] = (in[i]->gl_Position[0] + 1) * (state.image_width / 2);
-  		in[i]->gl_Position[1] = (in[i]->gl_Position[1] + 1) * (state.image_height / 2);	
-  		int image_index = in[i]->gl_Position[1] * state.image_width + in[i]->gl_Position[0];
-  		state.image_color[image_index] = make_pixel(255, 255, 255);	
+  		inn[i][0] = (inn[i][0] + 1) * (state.image_width / 2);
+  		inn[i][1] = (inn[i][1] + 1) * (state.image_height / 2);	
+  		int image_index = inn[i][1] * state.image_width + inn[i][0];
+  		state.image_color[image_index] = make_pixel(255, 255, 255);
+		for (int j = 0; j < state.floats_per_vertex; j++) {
+			std::cout << in[i]->data[j] << std::endl;
+		}
+		std::cout << "----------------------" << std::endl;  	
   	}
-	float area = 0.5 * ((in[0]->gl_Position[0] * (in[1]->gl_Position[1] - in[2]->gl_Position[1])) + 
-						          (in[1]->gl_Position[0] * (in[2]->gl_Position[1] - in[0]->gl_Position[1])) + 
-						          (in[2]->gl_Position[0] * (in[0]->gl_Position[1] - in[1]->gl_Position[1])));
-                                     
-  int startX = std::min(std::min(in[0]->gl_Position[0], in[1]->gl_Position[0]), in[2]->gl_Position[0]);
-  int endX = std::max(std::max(in[0]->gl_Position[0], in[1]->gl_Position[0]), in[2]->gl_Position[0]);
-  int startY = std::min(std::min(in[0]->gl_Position[1], in[1]->gl_Position[1]), in[2]->gl_Position[1]);
-  int endY = std::max(std::max(in[0]->gl_Position[1], in[1]->gl_Position[1]), in[2]->gl_Position[1]);
+	float area = 0.5 * ((inn[0][0] * (inn[1][1] - inn[2][1])) + 
+			(inn[1][0] * (inn[2][1] - inn[0][1])) + 
+			(inn[2][0] * (inn[0][1] - inn[1][1])));                        
+  int startX = std::min(std::min(inn[0][0], inn[1][0]), inn[2][0]);
+  int endX = std::max(std::max(inn[0][0], inn[1][0]), inn[2][0]);
+  int startY = std::min(std::min(inn[0][1], inn[1][1]), inn[2][1]);
+  int endY = std::max(std::max(inn[0][1], inn[1][1]), inn[2][1]);
 
   for (int i = startX; i < endX; i++) {
       for (int j = startY; j < endY; j++) {
           	/*float Area = 0.5 * ((i * (in[1]->gl_Position[1] - in[2]->gl_Position[1])) + 
           						          (in[1]->gl_Position[0] * (in[2]->gl_Position[1] - j)) + 
           						          (in[2]->gl_Position[0] * (j - in[1]->gl_Position[1])));*/
-          	float Brea = 0.5 * ((in[0]->gl_Position[0] * (j - in[2]->gl_Position[1])) + 
-          						          (i * (in[2]->gl_Position[1] - in[0]->gl_Position[1])) + 
-          						          (in[2]->gl_Position[0] * (in[0]->gl_Position[1] - j)));
-          	float Grea = 0.5 * ((in[0]->gl_Position[0] * (in[1]->gl_Position[1] - j)) + 
-          						          (in[1]->gl_Position[0] * (j - in[0]->gl_Position[1])) + 
-          						          (i * (in[0]->gl_Position[1] - in[1]->gl_Position[1])));
+          	float Brea = 0.5 * ((inn[0][0] * (j - inn[2][1])) + 
+          						          (i * (inn[2][1] - inn[0][1])) + 
+          						          (inn[2][0] * (inn[0][1] - j)));
+          	float Grea = 0.5 * ((inn[0][0] * (inn[1][1] - j)) + 
+          						          (inn[1][0] * (j - inn[0][1])) + 
+          						          (i * (inn[0][1] - inn[1][1])));
            //double alpha = Area / area;
            double beta = Brea / area;
            double gamma = Grea / area;
            double alpha = (1 - beta) - gamma;
            if (alpha >= 0 && beta >= 0 && gamma >= 0 && (alpha + beta + gamma) <= 1.001) {
                int image_index = (j * state.image_width) + i;
-  		         state.image_color[image_index] = make_pixel(255, 255, 255);
+		state.image_color[image_index] = make_pixel(in[1]->data[state.floats_per_vertex - 3], in[1]->data[state.floats_per_vertex - 2], in[1]->data[state.floats_per_vertex - 1]);
+		/*float r = (alpha * in[0]->data[4]) + (beta * in[1]->data[4]) + (gamma * in[2]->data[4]);
+		float g = (alpha * in[0]->data[5]) + (beta * in[1]->data[5]) + (gamma * in[2]->data[5]);
+		float b = (alpha * in[0]->data[6]) + (beta * in[1]->data[6]) + (gamma * in[2]->data[6]);
+		state.image_color[image_index] = make_pixel(r, g, b);*/
            }
       }
   }
